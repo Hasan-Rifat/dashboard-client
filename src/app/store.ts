a36943c7e90f1c94b4782f3cb7568b71ed913542
Hasan-Rifat <developerhr000@gmail.com>
2023-09-27 16:10:54 +0600

    [redux setup done]

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+// Or from '@reduxjs/toolkit/query/react'
+import { apiSlice } from "./api/apiSlice";
+
+export const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
